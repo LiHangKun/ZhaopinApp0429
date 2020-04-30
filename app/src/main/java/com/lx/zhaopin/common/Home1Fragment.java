834e709f6191051e76d180b0e732c393cb3616c7
cwl1992 <2571695806@qq.com>
2020-04-30 11:50:39 +0800

    写布局0430文字展示在中间

--- app/src/main/java/com/lx/zhaopin/common/Home1Fragment.java
+++ app/src/main/java/com/lx/zhaopin/common/Home1Fragment.java
@@ -3,11 +3,13 @@ package com.lx.zhaopin.common;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.lx.zhaopin.R;
 import com.lx.zhaopin.base.BaseFragment;
 
 public class Home1Fragment extends BaseFragment {
@@ -16,6 +18,9 @@ public class Home1Fragment extends BaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         TextView textView = new TextView(container.getContext());
+        textView.setTextSize(50);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(container.getContext().getResources().getColor(R.color.mainColor));
         textView.setText("1");
 
         return textView;
